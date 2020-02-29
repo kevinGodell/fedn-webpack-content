e38512c571bea8112dc2bd5172bd5c052b441ca0
Kevin Godell <kevin.godell@gmail.com>
2020-02-29 12:22:57 -0600

    convert to async await

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,16 +4,25 @@ const saySomething = val => {
   console.log(val);
 };
 
-const submitForm = e => {
+const submitForm = async e => {
   e.preventDefault();
   const form = e.currentTarget;
   const { elements, action, method } = form;
-  fetch(action)
+  /* fetch(action)
     .then(res => res.json())
     .then(function(res) {
       console.log(res.message);
       document.getElementById('results').innerHTML = res.message;
-    });
+    });*/
+  try {
+    const res = await fetch(action);
+    //console.log(res);
+    const json = await res.json();
+    //console.log(json);
+    document.getElementById('results').innerHTML = json.message;
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 export { saySomething, submitForm };
