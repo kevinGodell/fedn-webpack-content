db719a6d633a58817a209cfc8e9698d4a24e66b6
Kevin Godell <kevin.godell@gmail.com>
2020-02-29 12:23:56 -0600

    support polyfill

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,17 @@
+'use strict';
+
 module.exports = {
-  presets: ['@babel/preset-env']
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          browsers: ['> 1%', 'not dead'/*, 'not ie 11'*/]
+        },
+        useBuiltIns: 'usage',
+        corejs: 3,
+        debug: true
+      }
+    ]
+  ]
 };
